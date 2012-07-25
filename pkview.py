717b5ae5d96ce9e152915aceae7e600b4f24cea2
Sam <sam@s-blu.de>
2012-07-25 20:47:59 +0200

    version changed

--- pkview.py
+++ pkview.py
@@ -21,7 +21,7 @@ import sqlite3
 import pkdao
 import re
 
-ver = '0.4F'
+ver = '0.4.1'
 
 #Validiert die Existenz des Pokemon. Ist es existent, wird der Name ohne Leerzeichen und mit grossen Anfangsbuchstaben
 # (Datenbankkonform) zurueckgegeben
